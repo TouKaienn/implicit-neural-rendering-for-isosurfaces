6d2a58b6aa21af130357582aa3c206de98a59bd2
Dai-ge <1304059736@qq.com>
2021-07-21 21:13:01 +0800

    clean the code and rewrite most of the function

--- train.py
+++ train.py
@@ -9,26 +9,31 @@ import os
 from tqdm import tqdm
 from torch.nn import MSELoss
 import time
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from datetime import datetime
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Train():
     def __init__(self,config):
-        self.epochs=10
+        self.epochs=1
         self.time=time.time()
         #############################
         data = TrainDataset(text_path='.\\tiny_vorts0008_normalize_dataset\\vorts0008_infos.txt',img_root_path='.\\tiny_vorts0008_normalize_dataset')
         self.dataloader = DataLoader(data, batch_size=1, pin_memory=True, num_workers=0)
-        self.net = Siren(in_features=5, out_features=1, hidden_features=50, 
-                        hidden_layers=3, outermost_linear=True)
+        if not os.path.exists('.\\net.pkl'):
+            self.net = Siren(in_features=5, out_features=3, hidden_features=50, 
+                            hidden_layers=3, outermost_linear=True)
+        else:
+            self.load_model()
+        self.net.to(device=device)
         self.optimizer=torch.optim.Adam(lr=1e-4,params=self.net.parameters())
         self.loss_fuc=MSELoss()
-        # self.net.to(device=device)
+        self.loss=None
+
         
     def train(self):
         print("Start Training:")
-        for epoch in tqdm(range(self.epochs)):
+        for epoch in range(self.epochs):
             
             self.epoch=epoch
             for _, data in enumerate(self.dataloader):
@@ -36,40 +41,39 @@ class Train():
                 height,width = label_img.shape[1],label_img.shape[2]
                 self.train_one_img(label_img,txt_data,width,height)
 
-
-
-
-
     def train_one_img(self,label_img,txt_data,width,height):
-        for x in range(width):
+        for x in tqdm(range(width)):
             for y in range(height):
                 input_data=torch.cat((*txt_data,torch.tensor([x]),torch.tensor([y])))
                 ground_truth=label_img[0][y][x]
 
-                # input_data.to(device=device)
-                # ground_truth.to(device=device)
+                input_data,ground_truth=self.prepare(input_data,ground_truth)
 
                 output,_=self.net(input_data)
- 
-                loss=self.loss_fuc(output.to(torch.float32),ground_truth.to(torch.float32))
+                
+                self.loss=self.loss_fuc(output,ground_truth)
                 self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.setp()
+                self.loss.backward()
+                self.optimizer.step()
+            
+            self.save_model()#!记得删了
+            self.write_log(self.loss)
     
     def write_log(self,loss):
-        with open('log.txt','a') as f:
-            f.write(f'epoch:{self.epoch}, loss:{loss},time_consuming:{time.time()-self.time}s')
+        with open(f'log{datetime.now().strftime("%m%d")}.txt','a') as f:
+            f.write(f'epoch:{self.epoch}, time:{datetime.now().strftime("%m/%d_%H:%M:%S")}, loss:{loss},  time_consuming:{time.time()-self.time:.2f}s\n')
         self.time=time.time()
 
-                
-
-        
-
-
-
-
-
-
+    def prepare(self,*args):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if len(args)>1:
+            return (a.float().to(device) for a in args)
+    
+    def save_model(self):
+        torch.save(self.net,'net.pkl')
+    
+    def load_model(self):
+        self.net=torch.load('.\\net.pkl')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -82,5 +86,6 @@ if __name__ == "__main__":
 
     train=Train(config)
     train.train()
-    
+
+    # print(datetime.now().strftime("%m/%d_%H:%M"))
 
