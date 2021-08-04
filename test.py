940547ae239dbd09540f324319d2ffda6ed938fc
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-04 11:33:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -121,12 +121,12 @@ class Test():
 
 
     def test_one_img(self, label_img, txt_data, length):
-        
+
         if self.matrix is None:
             self.matrix = self.matrix_x_y(length)
         else:
             self.matrix = self.matrix
-        
+
         with torch.no_grad():
             # obtain three values (isovalue, alpha, beta)
             v1, v2, v3 = txt_data[0], txt_data[1], txt_data[2]
@@ -165,12 +165,9 @@ class Test():
 
     def visualize(self, output):
         fig, axes = plt.subplots(1, 1, figsize=(6, 6))
-        #print("output is", output)
-        axes.imshow(output.cpu().detach().numpy().transpose(1,2,0))
-        #print("outout for vis is", output.cpu().detach().numpy().transpose(1,2,0))
-        plt.savefig('recent.png')
-
-        sys.exit(0)
+        output = output.permute(1, 2, 0)  # permute before show
+        axes.imshow(output.cpu().detach().numpy())
+        plt.savefig("recent.png")
 
 
 
