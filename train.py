cd3ab41575eac15e8f0cdd165a9e98279e47f5e9
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-09 10:33:51 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from model import *
+# -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 from model import *
@@ -14,22 +14,30 @@ from datetime import datetime
 import numpy as np
 import sys
 
+from utils import *
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Trainer():
     def __init__(self):
-        self.epochs = 1000
+        self.epochs = config.epoch
         self.time = time.time()
         self.batch_size = config.batch_size
         #############################
         data = TrainDataset(config.save_txt_root_path,
-                            config.input_image,reload_bool=True,sidelen=512)
+                            config.input_image,
+                            reload_bool=True,
+                            sidelen=config.side_len)
         #self.input_data_FilePath=data.input_data_FilePath
         self.dataloader = DataLoader(data, batch_size=self.batch_size)
         #if not os.path.exists('.\\net.pkl') or True:
         if not os.path.exists('.\\net.pkl'):
-            self.net = Siren(in_features=81, out_features=3, hidden_features=50,
-                             hidden_layers=3, outermost_linear=True)  # in_features changed to 81 after pos_encoding. Original is 5
+            self.net = Siren(in_features=config.in_features,
+                             out_features=config.out_features,
+                             hidden_features=config.hidden_features,
+                             hidden_layers=config.hidden_layers,
+                             outermost_linear=config.outermost_linear,
+                             use_residual=config.use_residual)
         else:
             self.load_model()
         self.losses=[]
@@ -37,6 +45,8 @@ class Trainer():
         self.optimizer = torch.optim.Adam(lr=1e-3, params=self.net.parameters())
         self.scheduler=torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(self.optimizer,T_0=50,T_mult=5,eta_min=1e-6)
         self.loss_fuc = MSELoss().to(device)
+        self.PSNR = PSNR()
+        self.SSIM = SSIM()
         self.loss = None
 
     def train(self):
@@ -52,7 +62,12 @@ class Trainer():
                 output,_=self.net(input_data)
                 output = output.permute(0,3,1,2)
 
-                self.loss=self.loss_fuc(output,label)
+                #print("output.size()", output.size())
+                #print("label.size()", label.size())
+                self.psnr = self.PSNR(output, label)
+                self.ssim = self.SSIM(output, label)
+                self.loss = self.loss_fuc(output,label)
+
                 self.optimizer.zero_grad()
                 self.loss.backward()
                 self.optimizer.step()
@@ -60,7 +75,7 @@ class Trainer():
                 loss_cpu=self.loss.cpu().detach()
                 self.losses.append(loss_cpu)
                 self.visualize(output,label)
-                self.write_log(self.loss)
+                self.write_log(self.loss, self.psnr, self.ssim)
 
                 self.scheduler.step()
                 if loss_cpu==min(self.losses):
@@ -86,11 +101,11 @@ class Trainer():
         plt.ylabel('loss')
         plt.savefig("loss_curve"+ ".png")
         
-    def write_log(self, loss):
+    def write_log(self, loss, psnr, ssim):
         lr=self.optimizer.param_groups[-1]['lr']
         with open(f'log{datetime.now().strftime("%m%d")}.txt', 'a') as f:
             f.write(
-                f'epoch:{self.epoch}, time:{datetime.now().strftime("%m/%d_%H:%M:%S")}, lr:{lr:5f}, loss:{loss},  '
+                f'epoch:{self.epoch}, time:{datetime.now().strftime("%m/%d_%H:%M:%S")}, lr:{lr:5f}, loss:{loss}, psnr:{psnr}, ssim:{ssim}, '
                 f'time_consuming:{time.time() - self.time:.2f}s\n')
         self.time = time.time()
 
