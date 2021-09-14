2712886f9c9316a2fe5175133759cdcd4e25c6c1
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-09-15 00:03:19 +0800

    Update option.py

--- option.py
+++ option.py
@@ -5,26 +5,33 @@ parser = argparse.ArgumentParser()
 
 # This is for training and testing data
 parser.add_argument('--save_txt_root_path', type=str,
-                    default='D:/Code/SummerResearch/implicit-neural-rendering-for-isosurfaces-main/data/train_data')
+                    default='./data/train_data')
 parser.add_argument('--save_txt_root_path_test', type = str,
-                    default = 'D:/Code/SummerResearch/implicit-neural-rendering-for-isosurfaces-main/data/test_data')
+                    default = './data/test_data')
 parser.add_argument('--input_image', type=str,
-                    default='D:/Code/SummerResearch/implicit-neural-rendering-for-isosurfaces-main/data/train_data')
+                    default='./data/train_data')
 parser.add_argument('--input_image_test', type=str,
-                    default='D:/Code/SummerResearch/implicit-neural-rendering-for-isosurfaces-main/data/test_data')
+                    default='./data/test_data')
 
 # This is for training hyperparameters
-parser.add_argument('--epoch', type=int, default=100) # training epochs, default is 10000
+parser.add_argument('--epoch', type=int, default=501) # training epochs, default is 10000
 parser.add_argument('--side_len', type=int, default=512)
-parser.add_argument('--batch_size', type = int, default=10)
+parser.add_argument('--batch_size', type = int, default=120000)
+parser.add_argument('--lr_rate', type = float, default=5e-5)
 parser.add_argument('--train_test', type = str, default='train') # choose 'train' or 'test'
+parser.add_argument('--train_weight', type = str, default='net.pkl') # weight file from which to continue training
+parser.add_argument('--use_scheduler', type = bool, default=True)
 
 # This is for the model
 parser.add_argument('--outermost_linear', type = bool, default=True)
 parser.add_argument('--use_residual', type = bool, default=True)
-parser.add_argument('--in_features', type = int, default=81)
+parser.add_argument('--in_features', type = int, default=5)
 parser.add_argument('--out_features', type = int, default=3)
-parser.add_argument('--hidden_features', type = int, default=50)
-parser.add_argument('--hidden_layers', type = int, default=3) # This params needs tuning
+parser.add_argument('--init_features', type = int, default=256) # original is 128
+parser.add_argument('--num_res', type = int, default=10) # original is 10
+
+
+# This is for testing parameters
+parser.add_argument('--test_weight', type = str, default='net.pkl') # weight file name
 
 config = parser.parse_args()
